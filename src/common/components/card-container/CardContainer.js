0bce2a8aa6d66a9d3473a74d332ae874f9955613
Sanyam Jain <sanyamjaino98@gmail.com>
2022-12-07 00:32:37 +0530

    Tests: Card Container, Button

--- src/common/components/card-container/CardContainer.js
+++ src/common/components/card-container/CardContainer.js
@@ -1,8 +1,8 @@
-import React from 'react';
-import PropTypes from 'prop-types';
+import React from "react";
+import PropTypes from "prop-types";
 
-import Card from 'common/components/card/Card';
-import 'common/components/card-container/card-container.styles.scss';
+import Card from "common/components/card/Card";
+import "common/components/card-container/card-container.styles.scss";
 
 function CardContainer({
   title,
@@ -14,37 +14,40 @@ function CardContainer({
 }) {
   // determine class for container
   let cardContainerClassName = grid
-    ? 'card-container-grid'
-    : 'card-container-flex';
+    ? "card-container-grid"
+    : "card-container-flex";
 
   // determine class for title
   let titleClasses = [];
-  if (onTitleClick) titleClasses.push('card-container-title');
-  titlePosition === 'left'
-    ? titleClasses.push('card-container-title-position-left')
-    : titleClasses.push('card-container-title-position-center');
+  if (onTitleClick) titleClasses.push("card-container-title");
+  titlePosition === "left"
+    ? titleClasses.push("card-container-title-position-left")
+    : titleClasses.push("card-container-title-position-center");
 
   if (!onTitleClick) onTitleClick = () => {};
+
+  const formattedTitle = title ? title.charAt(0).toUpperCase() + title.slice(1) : ''
 
   return (
     <>
       {title && (
-        <h2 onClick={onTitleClick} className={titleClasses.join(' ')}>
-          {title.charAt(0).toUpperCase() + title.slice(1)}
+        <h2 onClick={onTitleClick} className={titleClasses.join(" ")}>
+          {formattedTitle}
         </h2>
       )}
-      <div className={cardContainerClassName}>
-        {cards.length !== 0 &&
-          cards.map((card) => (
+      {cards && cards.length !== 0 && (
+        <div className={cardContainerClassName} test-id='card-container'>
+          {cards.map((card) => (
             <Card key={card.id} cardData={card} large={large} />
           ))}
-      </div>
+        </div>
+      )}
     </>
   );
 }
 
 CardContainer.propTypes = {
-  cards: PropTypes.arrayOf(PropTypes.object).isRequired,
+  cards: PropTypes.arrayOf(PropTypes.object),
   title: PropTypes.string,
   grid: PropTypes.bool,
   large: PropTypes.bool,
