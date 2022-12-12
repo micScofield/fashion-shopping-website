8f595d74d324d5cdf63edd8ebb12b2c36bf9a8a2
Sanyam Jain <sanyamjaino98@gmail.com>
2022-12-12 05:52:55 +0530

    Adding stripe support for payments

--- src/common/components/card-container/CardContainer.test.js
+++ src/common/components/card-container/CardContainer.test.js
@@ -1,20 +1,40 @@
-import { shallow } from 'enzyme'
-import { v4 } from 'uuid'
+import { shallow } from "enzyme";
+import { v4 } from "uuid";
 
-import CardContainer from './CardContainer'
+import CardContainer from "./CardContainer";
 
-const generateCard = () => ({ id: v4() })
+const generateCard = () => ({ id: v4() });
 
-it('renders all the cards passed to it', () => {
-    const wrapper = shallow(<CardContainer cards={[generateCard(), generateCard()]} />)
-    expect(wrapper.find('[test-id="card-container"]').children().length).toEqual(2)
-})
+it("renders all the cards passed to it", () => {
+  const wrapper = shallow(
+    <CardContainer cards={[generateCard(), generateCard()]} />
+  );
+  expect(wrapper.find('[test-id="card-container"]').children().length).toEqual(
+    2
+  );
+});
 
-it('renders a title if a title prop is passed to it', () => {
-    const title = 'Title'
-    const wrapper = shallow(<CardContainer cards={[generateCard()]} title={title} />)
+describe("card container title", () => {
+  it("renders a title if a title prop is passed to it", () => {
+    const title = "Title";
+    const wrapper = shallow(
+      <CardContainer cards={[generateCard()]} title={title} />
+    );
 
-    const formattedTitle = title.charAt(0).toUpperCase() + title.slice(1)
+    const formattedTitle = title.charAt(0).toUpperCase() + title.slice(1);
 
-    expect(wrapper.children().containsMatchingElement(<h2>{formattedTitle}</h2>)).toBeTruthy()
-})
+    expect(
+      wrapper.children().containsMatchingElement(<h2>{formattedTitle}</h2>)
+    ).toBeTruthy();
+  });
+
+  it("doesn't render a title if no title prop is passed to it", () => {
+    const wrapper = shallow(<CardContainer cards={[generateCard()]} />);
+
+    const formattedTitle = "Title";
+
+    expect(
+      wrapper.children().containsMatchingElement(<h2>{formattedTitle}</h2>)
+    ).toBeFalsy();
+  });
+});
